03111f4c5a8d0554dcbaff35108d9979b0b6c517
ifrs <ifrs@192.168.56.1>
2017-06-14 22:11:20 -0300

    Erro

--- Integcontinua/src/integcontinua/Integcontinua.java
+++ Integcontinua/src/integcontinua/Integcontinua.java
@@ -17,7 +17,7 @@ public class Integcontinua {
      */
     public static void main(String[] args) {
         System.out.println("Teste");
-        System.out.println("Teste alteração");
+        System.out.println("Teste alteração")
     }
     
 }
